57569fd5c32c83aa04404c68f1405418e3650652
Radovan Dvorsky <radovan.dvorsky@gmail.com>
2025-02-20 16:38:29 +0100

    Updated scopus.py

--- scopus.py
+++ scopus.py
@@ -23,8 +23,6 @@ import requests
 
 import sys
 
-sys.path.append('C:/Python/')
-#sys.path.append('/mnt/e/python/')
 from nutils import ExcelTable
 
 #headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
